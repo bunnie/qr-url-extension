fa3b74ff487b22d6cc1afc54b1b1ac1ed1bea8b4
hanatonikkou <github@spamgourmet.com>
2025-06-15 12:42:20 +0200

    Embed current time in QR code

--- popup.js
+++ popup.js
@@ -5,6 +5,30 @@ document.addEventListener('DOMContentLoaded', function () {
     if (!domain) {
       document.getElementById('domain').textContent = "Page has an invalid URL";
     };
+    let currentTime = new Date();
+    const timezoneMarker = (currentTime.getTimezoneOffset()/60>0 ? "-": "+")+
+                           (currentTime.getTimezoneOffset()/60<10 ? "0" : "")+
+                           (Math.abs(currentTime.getTimezoneOffset()/60))+":00";
+
+    // Interval set to 200ms to limit the maximum offset between system time and time displayed 
+    setInterval(()=>{
+      // .toISOString() returns UTC zero offset time, not local time
+      // Solution: subtract local offset from current time, format with .toISOString(), and add timezone marker
+      // .slice() removes milliseconds and trailing Z, which indicates UTC zero offset time
+      currentTime = new Date();
+      let currentTimeOffsetSubtracted = currentTime.getTime() - currentTime.getTimezoneOffset() * 60 * 1000;
+      let localCurrentTime = new Date(currentTimeOffsetSubtracted);
+      let currentTimeString = localCurrentTime.toISOString().slice(0, -5) + timezoneMarker;
+      
+      let qrContent = "{ domain: " + domain + " time: " + currentTimeString + " }";
+      // console.log(qrContent);
+      new QRious({
+        element: document.getElementById('qr'),
+        value: qrContent,
+        size: 180
+      });
+    }, 200)
+
     new QRious({
       element: document.getElementById('qr'),
       value: domain,
